f882db96a54850be7c6ad4963f85752e53c7c82e
Wenchao Lu <wenchao.lu@philips.com>
2016-10-09 10:09:47 +0800

    Save the project
    Signed-off-by: Wenchao Lu <wenchao.lu@philips.com>

---
+++ 1.bat
@@ -0,0 +1,13 @@
+
+"C:\Program Files (x86)\SEGGER\JLinkARM_V458a\jlink.exe" a.jlink
+
+compare.sh
+
+if res=="Passed"
+@echo "Passed"
+
+pause
+else
+@echo "Failed"
+
+pause
--- compare.sh
+++ compare.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-origin_file="D:\work\SMARTnext_sensor\bin\sensor,bin"
+origin_file="D:\work\SMARTnext_sensor\bin\sensor.bin"
 read_file="D:\work\SMARTnext_sensor\bin\1.bin"
 
 if [ -f ${read_file} ]; then
-	read_bin=`awk {'print $0'} ${read_file}`
+	read_bin=`xxd -l 20 ${read_file}`
 fi
 
 if [ -f ${origin_file} ]; then
-	origin_bin=`awk {'print $0'} ${origin_file} | sed 'l/\20'`
+	origin_bin=`xxd -l 20 ${origin_file}`
 fi
 
 if [ "${read_bin}" == "${origin_bin}" ]; then
-	echo "Unsecurity"
+	echo "passed"
+else
+        echo "failed"
 fi
+read -p "Press any key to continue." var
