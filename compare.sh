2c38b2719a40bd33a12f7279e2e5920888cbaeee
Wenchao Lu <wenchao.lu@philips.com>
2016-10-10 10:09:43 +0800

    Add while to verify the high 16 bits 1.The high 16bits mustbe 0010
    Signed-off-by: Wenchao Lu <wenchao.lu@philips.com>

--- compare.sh
+++ compare.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-origin_file="D:\work\SMARTnext_sensor\bin\sensor.bin"
 read_file="D:\work\SMARTnext_sensor\bin\1.bin"
+i=2
 set a -a
+for((i=2;i<10;i=i+2))
+do
 if [ -f ${read_file} ]; then
-	read_bin=`xxd -l 20 ${read_file}`
+	read_bin=`xxd -l 2 -c 2 -s $i -ps ${read_file}`
 fi
-
-if [ -f ${origin_file} ]; then
-	origin_bin=`xxd -l 20 ${origin_file}`
-fi
-
-if [ "${read_bin}" == "${origin_bin}" ]; then
+if [ "${read_bin}" == "0010" ]; then
 	echo 1
 	a=0
 	export a
@@ -21,5 +18,6 @@ else
 	a=1
         export a
 	read -p "Press any key to continue." var
-        exit 1
+        continue;
 fi
+done
