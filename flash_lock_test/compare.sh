1147159bc421061aef1f8f8c3d7be0c786b2de59
Josh Wu <josh.wu@philips.com>
2016-10-10 10:18:23 +0800

    add offset's memory value check
    Signed-off-by: Josh Wu <josh.wu@philips.com>

--- flash_lock_test/compare.sh
+++ flash_lock_test/compare.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
-origin_file="D:\work\SMARTnext_sensor\bin\sensor.bin"
-read_file="D:\work\SMARTnext_sensor\bin\1.bin"
+read_file="mem.bin"
 
+# offset of memory
+OFFSET_MEM="4 8 16 20 28"
+
+# values in the offset
+declare -A offset_val=(
+			["4"]="d5000010"
+			["8"]="e3000010"
+			["16"]="e7000010"
+			["20"]="e9000010"
+			["28"]="00000000"
+			)
+
+result="PASS"
 if [ -f ${read_file} ]; then
-	read_bin=`xxd -l 20 ${read_file}`
+	# check the value of the responding offset
+	for OFFSET in $OFFSET_MEM
+	do
+		read_val=`xxd -s ${OFFSET} -l 4 -p ${read_file}`
+		#echo $read_val
+		if [ "${read_val}" != "${offset_val["${OFFSET}"]}" ]; then
+			result="FAIL"
+			break
+		fi
+	done
 fi
 
-if [ -f ${origin_file} ]; then
-	origin_bin=`xxd -l 20 ${origin_file}`
-fi
-
-if [ "${read_bin}" == "${origin_bin}" ]; then
+if [ "x${result}" == "xPASS" ]; then
 	echo "passed"
 else
-        echo "failed"
+    echo "failed"
 fi
-read -p "Press any key to continue." var
+
+read -p "Press any key to continue..." var
+
